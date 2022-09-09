fc19f760f9b17baff421d7c97c7a5296b91247de
DESKTOP-29V3F4G\asd <thd67371@naver.com>
2022-09-09 20:31:49 +0900

    카페등록(이미지업로드)

--- kikazProject/src/main/java/com/kikaz/project/model/Company.java
+++ kikazProject/src/main/java/com/kikaz/project/model/Company.java
@@ -1,6 +1,8 @@
 package com.kikaz.project.model;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
@@ -10,8 +12,10 @@ import lombok.Data;
 @Entity
 @Table
 public class Company {
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	@Id
 	private Long company_id;
+	private String company_name;
 	private String business_name;
 	private String location;
 	private String com_imgpath;
