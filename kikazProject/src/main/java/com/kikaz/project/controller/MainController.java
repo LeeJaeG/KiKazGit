3d027a2d5f3335e5608916f14895f71f4b81b250
DESKTOP-D0BMODR\15 <wlrmworms@naver.com>
2022-09-09 15:00:46 +0900

    -join

--- kikazProject/src/main/java/com/kikaz/project/controller/MainController.java
+++ kikazProject/src/main/java/com/kikaz/project/controller/MainController.java
@@ -1,16 +1,47 @@
 package com.kikaz.project.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+import com.kikaz.project.model.Role;
+import com.kikaz.project.model.User;
+import com.kikaz.project.repository.UserRepository;
+
+
+
+
+
+@Controller
 public class MainController {
 
-	@GetMapping("/main")
+	@Autowired
+	private UserRepository userRepository;
+	@Autowired
+	private BCryptPasswordEncoder bCryptPasswordEncoder;
+	
+	@GetMapping("/loginForm")
 	public String home() {
-		return "123";
+		return "loginForm";
 	}
 	
+	@GetMapping("/joinForm")
+	public String main() {
+		return "joinForm";
+	}
+	
+	@PostMapping("/join")
+	public String join(User user) {
+		System.out.println(user.toString());
+		//userRepository.save(user);
+		user.setRole(Role.USER);
+		String encPassword = bCryptPasswordEncoder.encode(user.getPassword());
+		user.setPassword(encPassword);
+		userRepository.save(user);
+		return "redirect:/loginForm";
+	}
 
 }
