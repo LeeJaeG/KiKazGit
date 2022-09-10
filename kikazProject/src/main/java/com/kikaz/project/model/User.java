dd3871df3f9f5cb7d150926efbdad9ea254c98bc
DESKTOP-QSF1OJC\17 <bbqskgud@naver.com>
2022-09-10 14:15:09 +0900

    Merge branch 'nh_bran'
    # Conflicts:
    #	kikazProject/src/main/java/com/kikaz/project/config/SecurityConfig.java
    #	kikazProject/src/main/java/com/kikaz/project/model/User.java
    #	kikazProject/src/main/java/com/kikaz/project/repository/UserRepository.java
    #	kikazProject/src/main/resources/templates/joinForm.html
    #	kikazProject/src/main/resources/templates/loginForm.html

--- kikazProject/src/main/java/com/kikaz/project/model/User.java
+++ kikazProject/src/main/java/com/kikaz/project/model/User.java
@@ -2,12 +2,12 @@ package com.kikaz.project.model;
 
 import java.util.Date;
 
-import javax.persistence.Column;
+
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
+
+
 import javax.persistence.Id;
 import javax.persistence.Table;
 
@@ -17,6 +17,7 @@ import lombok.Data;
 @Entity
 @Table
 public class User {
+	
 	@Id
 	private String username;
 	private String u_name;
--- kikazProject/src/main/java/com/kikaz/project/repository/UserRepository.java
+++ kikazProject/src/main/java/com/kikaz/project/repository/UserRepository.java
@@ -2,12 +2,11 @@ package com.kikaz.project.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-
 import com.kikaz.project.model.User;
 
 
 public interface UserRepository extends JpaRepository<User, Long>  {
 
 	public User findByUsername(String username);
-	
+
 }
