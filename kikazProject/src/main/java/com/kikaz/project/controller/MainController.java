ea97ae2cbab3423e76cc52e38aaaf4b90e00946a
DESKTOP-29V3F4G\asd <thd67371@naver.com>
2022-09-10 15:17:48 +0900

    section

--- kikazProject/src/main/java/com/kikaz/project/controller/MainController.java
+++ kikazProject/src/main/java/com/kikaz/project/controller/MainController.java
@@ -3,12 +3,21 @@ package com.kikaz.project.controller;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.propertyeditors.CustomDateEditor;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.ServletRequestDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
-
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,8 +27,10 @@ import org.springframework.web.multipart.MultipartFile;
 
 import com.kikaz.project.model.Company;
 import com.kikaz.project.model.Role;
+import com.kikaz.project.model.Section;
 import com.kikaz.project.model.User;
 import com.kikaz.project.repository.CompanyRepository;
+import com.kikaz.project.repository.SectionRepository;
 import com.kikaz.project.repository.UserRepository;
 
 @Controller
@@ -31,7 +42,17 @@ public class MainController {
 	private BCryptPasswordEncoder bCryptPasswordEncoder;
 	@Autowired
 	private CompanyRepository companyrepositiry;
+	@Autowired
+	private SectionRepository sectionRepository;
 
+	
+	@InitBinder
+    protected void init(HttpServletRequest request, ServletRequestDataBinder binder) {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss");
+        dateFormat.setLenient(false);
+        binder.registerCustomEditor(Date.class, new CustomDateEditor(dateFormat, false));
+    }
+	
 	@GetMapping("/loginForm")
 	public String home() {
 		return "loginForm";
@@ -54,6 +75,7 @@ public class MainController {
 
 	}
 
+	// 키즈카페 등록
 	@GetMapping("/cafeinsert")
 	public String cafejoin() {
 		return "cafeinsert";
@@ -72,6 +94,29 @@ public class MainController {
 
 		}
 		return "cafesuccess";
+	}
+
+	// 파트 등록
+	@GetMapping("/sectioninsert")
+	public String sectionjoin() {
+		return "sectioninsert";
+	}
+	
+	@PostMapping("/s_insert")
+	public @ResponseBody String s_insert(MultipartFile file, @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)Section sect ) {
+		String imageFileName = file.getOriginalFilename();
+		String path = "D:/sectionfile/" + imageFileName;
+		sect.setSect_imgpath(path);
+		sectionRepository.save(sect);
+		Path sectPath = Paths.get(path + imageFileName);
+		try {
+			Files.write(sectPath, file.getBytes());
+		} catch (Exception e) {
+
+		}
+
+		return "cafesuccess";
+
 	}
 
 }
--- kikazProject/src/main/java/com/kikaz/project/model/Section.java
+++ kikazProject/src/main/java/com/kikaz/project/model/Section.java
@@ -1,14 +1,17 @@
 package com.kikaz.project.model;
 
+
 import java.util.Date;
 
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
-import javax.persistence.OneToOne;
 import javax.persistence.Table;
+
+import org.springframework.format.annotation.DateTimeFormat;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
@@ -20,11 +23,15 @@ import lombok.Data;
 public class Section {
 
 	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Long section_id;
 	private String section_name;
 	private int price;
+	@DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
 	private Date standard_time;
+	@DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
 	private Date start_time;
+	@DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
 	private Date end_time;
 	private int maxnum_adult;
 	private int maxnum_child;
