06903a5769caeb7d928ff764cfd0c95318e21847
DESKTOP-29V3F4G\asd <thd67371@naver.com>
2022-09-11 20:13:45 +0900

    예약 조회 추가

--- kikazProject/src/main/java/com/kikaz/project/controller/ReservationController.java
+++ kikazProject/src/main/java/com/kikaz/project/controller/ReservationController.java
@@ -1,16 +1,31 @@
 package com.kikaz.project.controller;
 
+import java.security.Principal;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.kikaz.project.model.Reservation;
+import com.kikaz.project.model.User;
 import com.kikaz.project.repository.ReservationRepository;
+import com.kikaz.project.repository.UserRepository;
+
+
 
 @Controller
 public class ReservationController {
@@ -18,23 +33,38 @@ public class ReservationController {
 	@Autowired
 	private ReservationRepository reservationrepositiry;
 	
+	@Autowired
+	private UserRepository userRepository;
+	
 	@GetMapping("/reservation")
 	public String resevation() {
+		
 		return "reservation";
 	}
 
 	@PostMapping("/r_insert")
 	@ResponseBody
-	public ResponseEntity<Reservation> postEating(@RequestBody Reservation sleepData) {
-		System.out.println("post request");
-		System.out.println(sleepData.toString());
-		reservationrepositiry.save(sleepData);
-		return new ResponseEntity<Reservation>(sleepData, HttpStatus.CREATED);
+	public ResponseEntity<Reservation> postEating(@RequestBody Reservation reservation, Principal principal) {
+		User user = userRepository.findByUsername(principal.getName());
+		 System.out.println(user);
+		reservation.setUser(user);
+		reservationrepositiry.save(reservation);
+		return new ResponseEntity<Reservation>(reservation, HttpStatus.CREATED);
 		}
-
-	@GetMapping("/r_list")
-	public String r_list() {
-		return "reservationlist";
+	
+	//아이디 기준으로 리스트 출력
+	@RequestMapping("/r_listall")
+	public String Reservation(Model m, Principal principal) {
+		System.out.println("왔다");
+		User user = userRepository.findByUsername(principal.getName());
+		List<Reservation> reservation = reservationrepositiry.findAllByUser_Username(user.getUsername());
+		System.out.println( reservation);
+		m.addAttribute("reservation", reservation);
+		return "/reservationlist";
+		
 	}
+	
+	
+	
 	
 }
--- kikazProject/src/main/java/com/kikaz/project/model/Reservation.java
+++ kikazProject/src/main/java/com/kikaz/project/model/Reservation.java
@@ -1,7 +1,6 @@
 package com.kikaz.project.model;
 
-import java.util.Date;
-import java.sql.Timestamp;
+
 import java.time.LocalDateTime;
 import java.util.List;
 
@@ -14,10 +13,7 @@ import javax.persistence.Id;
 import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
-import javax.persistence.Temporal;
-import javax.persistence.TemporalType;
 
-import org.springframework.format.annotation.DateTimeFormat;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
@@ -35,10 +31,10 @@ public class Reservation {
 	private Boolean success_status;
 	private LocalDateTime rstart;
 	private LocalDateTime rend;
-	@ManyToMany(fetch=FetchType.LAZY, targetEntity = Section.class) //  클래스 명시
+	@ManyToOne(fetch=FetchType.LAZY, targetEntity = Section.class) //  클래스 명시
 	@JsonIgnore 
-	private List<Section> section;
+	private Section section;
 	@ManyToOne(fetch=FetchType.LAZY ,targetEntity = User.class)
 	@JsonIgnore
-	private List<User> user;
+	private User user;
 }
