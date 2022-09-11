23c2b13dad73f73c51aa3bea8a44f0919724f24a
DESKTOP-QSF1OJC\17 <bbqskgud@naver.com>
2022-09-11 12:41:46 +0900

    no message

--- kikazProject/src/main/java/com/kikaz/project/controller/MainController.java
+++ kikazProject/src/main/java/com/kikaz/project/controller/MainController.java
@@ -1,25 +1,32 @@
 package com.kikaz.project.controller;
 
+
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
+import java.util.Optional;
+
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.PostMapping;
 
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.kikaz.project.model.Company;
 import com.kikaz.project.model.Role;
+import com.kikaz.project.model.Section;
 import com.kikaz.project.model.User;
 import com.kikaz.project.repository.CompanyRepository;
+import com.kikaz.project.repository.SectionRepository;
 import com.kikaz.project.repository.UserRepository;
 
 @Controller
@@ -31,6 +38,8 @@ public class MainController {
 	private BCryptPasswordEncoder bCryptPasswordEncoder;
 	@Autowired
 	private CompanyRepository companyrepositiry;
+	@Autowired
+	private SectionRepository sectionRepository;
 
 	@GetMapping("/loginForm")
 	public String home() {
@@ -64,8 +73,9 @@ public class MainController {
 	@PostMapping("/c_insert")
 	public @ResponseBody String c_insert(MultipartFile file, Company com) {
 		String imageFileName = file.getOriginalFilename();
-		String path = "D:/projectfile/" + imageFileName;
-		com.setCom_imgpath(path);
+		System.out.println(imageFileName);
+		String path = "C:\\Users\\17\\git\\KiKazGit\\kikazProject\\src\\main\\resources\\static\\image\\";
+		com.setCom_imgpath(path+imageFileName);
 		companyrepositiry.save(com);
 		Path imaPath = Paths.get(path + imageFileName);
 		try {
@@ -75,5 +85,48 @@ public class MainController {
 		}
 		return "cafesuccess";
 	}
+	
+	@GetMapping("/sectioninsert")
+	   public String sectionjoin() {
+	      return "sectioninsert";
+	   }
+	   
+	   @PostMapping("/s_insert")
+	   public @ResponseBody String s_insert(MultipartFile file, @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)Section sect ) {
+	      System.out.println(sect);
+	      String imageFileName = file.getOriginalFilename();
+	      String path = "D:/sectionfile/";
+	      sect.setSect_imgpath(path);
+	      sectionRepository.save(sect);
+	      Path sectPath = Paths.get(path + imageFileName);
+	      try {
+	         Files.write(sectPath, file.getBytes());
+	      } catch (Exception e) {
 
+	      }
+
+	      return "cafesuccess";
+
+	   }
+	   
+		/*
+		 * @RequestMapping("/cafeimg") public String cafe_img(Model
+		 * mod,@RequestParam(value = "id", defaultValue = "0")Long id) {
+		 * Optional<Company> com = companyrepositiry.findById(id);
+		 * mod.addAttribute("com", com); System.out.println("====>"+com); return
+		 * "cafeimage"; }
+		 */
+	   @RequestMapping("/cafeimg")   
+	   public String SelectDummies(Model mod) {
+		   Long id = (long) 18;
+		   Optional<Company> result = companyrepositiry.findById(id);
+		   System.out.println("=============================");
+		   Company com = result.get();
+		   System.out.println(com);
+		   
+		   mod.addAttribute("com", com);
+		   return "cafeimage";
+	   }
+	  
+	   
 }
--- kikazProject/src/main/java/com/kikaz/project/model/Section.java
+++ kikazProject/src/main/java/com/kikaz/project/model/Section.java
@@ -1,14 +1,19 @@
 package com.kikaz.project.model;
 
+
 import java.util.Date;
 
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
-import javax.persistence.OneToOne;
 import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import org.springframework.format.annotation.DateTimeFormat;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
@@ -19,17 +24,26 @@ import lombok.Data;
 @Table
 public class Section {
 
-	@Id
-	private Long section_id;
-	private String section_name;
-	private int price;
-	private Date standard_time;
-	private Date start_time;
-	private Date end_time;
-	private int maxnum_adult;
-	private int maxnum_child;
-	private String sect_imgpath;
-	@ManyToOne(fetch=FetchType.LAZY)
-	@JsonIgnore
-	private Company company;
+   @Id
+   @GeneratedValue(strategy=GenerationType.IDENTITY)
+   private Long section_id;
+   private String section_name;
+   private int price;
+   
+   @DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
+   @Temporal(TemporalType.TIMESTAMP)
+   private Date standard_time;
+   @DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
+   @Temporal(TemporalType.TIMESTAMP)
+   private Date start_time;  
+   @DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
+   @Temporal(TemporalType.TIMESTAMP)
+   private Date end_time;
+   
+   private int maxnum_adult;
+   private int maxnum_child;
+   private String sect_imgpath;
+   @ManyToOne(fetch=FetchType.LAZY)
+   @JsonIgnore
+   private Company company;
 }
