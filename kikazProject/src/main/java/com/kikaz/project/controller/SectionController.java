6fcadb1d6271bfc35fd3fc464fe7e020679bb903
DESKTOP-D0BMODR\15 <wlrmworms@naver.com>
2022-09-14 09:32:00 +0900

    no message

--- kikazProject/src/main/java/com/kikaz/project/controller/SectionController.java
+++ kikazProject/src/main/java/com/kikaz/project/controller/SectionController.java
@@ -7,10 +7,14 @@ import java.time.LocalDateTime;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
@@ -19,6 +23,7 @@ import com.kikaz.project.model.Company;
 import com.kikaz.project.model.Section;
 import com.kikaz.project.repository.CompanyRepository;
 import com.kikaz.project.repository.SectionRepository;
+import com.kikaz.project.service.SectionService;
 
 
 @Controller
@@ -27,20 +32,23 @@ public class SectionController {
 	private SectionRepository sectionRepository;
 	@Autowired
 	private CompanyRepository companyrepositiry;
-	
-	
+	@Autowired
+	private SectionService sectionService;
+	private String companyid;
 	
 	@GetMapping("/sectioninsert")
-	public String sectionjoin(@RequestParam("companyid") String companyid) {
-		System.out.println(companyid);
+	public String sectionjoin(Model model,@RequestParam("companyid") String companyid) {
+		model.addAttribute("companyid",companyid);
 		return "sectioninsert";
 	}
 
-	@GetMapping("/s_insert")
-	public String s_insert(@RequestParam("file") MultipartFile file, Section sect,
+	@PostMapping("/s_insert")
+	public String s_insert(@RequestParam("file") MultipartFile file,@RequestParam(value="companyid") String companyid,
+			Section sect,
 			@RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime standard_time,
 			@RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime start_time,
 			@RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime end_time) {
+		System.out.println(companyid);
 		String imageFileName = file.getOriginalFilename();
 		String path = "C:\\Users\\15\\git\\KiKazGit\\kikazProject\\src\\main\\resources\\static\\sec_img\\";
 		sect.setSect_imgpath(imageFileName);
@@ -61,6 +69,16 @@ public class SectionController {
 		return "cafesuccess";
 
 	}
+	
+	@GetMapping("/s_list")
+	public String getList(Model model,
+			@PageableDefault(size = 5, sort = "sectionid", direction = Sort.Direction.DESC) Pageable pageable) {
+		sectionService.findBoardList(pageable);
+		model.addAttribute("companyList", sectionService.findBoardList(pageable));
+
+		return "sectionlist";
+	}
+	
 
 	@RequestMapping("/secimg")
 	public String Secimg(Model mod) {
